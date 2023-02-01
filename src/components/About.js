23a1969ed761a9d96513ddfd4f56fafe54fc1707
sudheesh-p <sudhianandhan@gmail.com>
2023-02-02 00:03:02 +0530

    class components

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
+import Profile from "./ProfileClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About page</h1>
-      <h3>This is react live course</h3>
-      <Outlet/>
-    </div>
-    
-  );
-};
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor Parent');
+  }
+
+  componentDidMount(){
+    console.log('Component did mount parent');
+  }
+
+  render() {
+    console.log("render parent");
+    return (
+      <div>
+        <h1>About page</h1>
+        <h3>This is react live course</h3>
+        {/* <Outlet/> */}
+        <Profile name={"First Child"} />
+        <Profile name={"Secound Child"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,9 +1,44 @@
 import React from "react";
 
 class Profile extends React.Component{
-
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+        console.log("constructor " + this.props.name)
+    }
+     
+    componentDidMount(){
+        //API calls
+        console.log('component did mount '+ this.props.name);
+        this.timer = setInterval(()=>{
+            console.log("Namaste OP")
+        },1000);
+    }
+    componentDidUpdate(){
+        console.log("component did update");
+    }
+    componentWillUnmount(){
+        console.log("component will unmount")
+        clearInterval(this.timer);
+    }
     render(){
-        return <h1>Profile Class Component</h1>
+        console.log('render ' + this.props.name);
+        return (
+            <div>
+                <h1>Profile Class Component</h1>
+                <h3>Name is {this.props.name}</h3>
+                <h4>Count is {this.state.count}</h4>
+                <button onClick={() =>{
+                    this.setState({
+                        count: 1
+                    })
+                }}>SetCount</button>
+            </div>
+        )
+        
+
     }
 
 }
